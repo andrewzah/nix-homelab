a01f234d3eedd545b21adfc3d3e738bcd0c62ea3
Andrew Zah <zah@andrewzah.com>
2025-02-19 18:32:54 +0900

    falcon/virt: don't bind to 22 yet

--- hosts/falcon/virtualisation.nix
+++ hosts/falcon/virtualisation.nix
@@ -31,7 +31,7 @@
       autoStart = true;
       image = "docker.io/library/traefik:v3.1.4@sha256:6215528042906b25f23fcf51cc5bdda29e078c6e84c237d4f59c00370cb68440";
       ports = [
-        "22:22"
+        #"22:22"
         "80:80"
         "443:443"
         "8080:8080"
