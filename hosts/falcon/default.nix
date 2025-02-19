5d19bf7e250f94725e3a69616c04f95ea244d4c0
Andrew Zah <zah@andrewzah.com>
2025-02-19 18:28:56 +0900

    add experimentals back to falcon

--- hosts/falcon/default.nix
+++ hosts/falcon/default.nix
@@ -4,6 +4,7 @@
 
     ./virtualisation.nix
   ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   sops.defaultSopsFile = ../../secrets.yaml;
   sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
