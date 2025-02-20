b491015d31609433ea899d473df3ca1fe2a038e2
Andrew Zah <zah@andrewzah.com>
2025-02-21 03:00:40 +0900

    f/smartd: daily/weekly testing

--- hosts/falcon/services/smartd.nix
+++ hosts/falcon/services/smartd.nix
@@ -18,6 +18,7 @@
 in {
   services.smartd = {
     enable = true;
+    defaults.monitored = "-a -o on -s (S/../.././02|L/../../7/04)";
     extraOptions = [
       "-w ${smartdNotify.outPath}"
     ];
