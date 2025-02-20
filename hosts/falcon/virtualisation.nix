043cb4e9639e4bc9ce2edafb801a932c927f831e
Andrew Zah <zah@andrewzah.com>
2025-02-21 00:31:16 +0900

    falcon/virt: better systemd oneshot script

--- hosts/falcon/virtualisation.nix
+++ hosts/falcon/virtualisation.nix
@@ -4,21 +4,34 @@
     docker.rootless.setSocketVariable = true;
     docker.autoPrune.enable = true;
     containerd.enable = true;
+
+    oci-containers.backend = "docker"; # defaults to podman
   };
 
   environment.sessionVariables = {
-    # for rootless docker
-    DOCKER_HOST = "unix:///run/docker.sock";
+    DOCKER_HOST = "unix:///run/docker.sock"; # fix for rootless docker
   };
 
-  # defaults to podman
-  virtualisation.oci-containers.backend = "docker";
+  systemd.services.create-docker-networks = {
+    description = "Create external and internal docker networks manually";
+    after = [ "network.target" ];
+    wants = [ "network-online.target" ];
+    script = ''
+      ${pkgs.docker}/bin/docker network create internal &2>/dev/null || true
+      ${pkgs.docker}/bin/docker network create external &2>/dev/null || true
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 
   # need to make networks manually in order
   # for containers to connect by hostname
   # e.g. postgres
-  system.activationScripts.mkDockerNetworks = ''
-    ${pkgs.docker}/bin/docker network create internal &2>/dev/null || true
-    ${pkgs.docker}/bin/docker network create external &2>/dev/null || true
-  '';
+  #system.activationScripts.mkDockerNetworks = ''
+  #  ${pkgs.docker}/bin/docker network create internal &2>/dev/null || true
+  #  ${pkgs.docker}/bin/docker network create external &2>/dev/null || true
+  #'';
 }
