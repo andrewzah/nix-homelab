df969cb550e76423836bc006e0577c1b53b4f54b
Andrew Zah <zah@andrewzah.com>
2025-03-15 16:13:34 +0900

    f/v: bumpity com.andrewzah, again

--- hosts/falcon/containers/com-andrewzah.nix
+++ hosts/falcon/containers/com-andrewzah.nix
@@ -1,7 +1,7 @@
 {...}: {
   virtualisation.oci-containers.containers.com-andrewzah = {
     autoStart = true;
-    image = "docker.io/andrewzah/com-andrewzah:latest@sha256:66422e4a9d2eb6d13d50ec4017541b557303020068638638e5d000a11bd72999";
+    image = "docker.io/andrewzah/com-andrewzah:latest@sha256:afa11d7cc30a6cf8a7133d33b1817d6fa0e6b277997409f0fd59744b1bf53dda";
     labels = {
       "traefik.enable" = "true";
       "traefik.http.routers.com-andrewzah.rule" = "Host(`andrewzah.com`)";
