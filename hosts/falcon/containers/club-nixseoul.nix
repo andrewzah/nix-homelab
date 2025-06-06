7cd154e4f0b1e65f507cc9683a7137fba9b39d87
Andrew Zah <zah@andrewzah.com>
2025-06-07 02:55:24 +0900

    bruh (x3)

--- hosts/falcon/containers/club-nixseoul.nix
+++ hosts/falcon/containers/club-nixseoul.nix
@@ -1,7 +1,7 @@
 {...}: {
   virtualisation.oci-containers.containers.club-nixseoul = {
     autoStart = true;
-    image = "docker.io/andrewzah/club-nixseoul:latest@sha256:31428f578bc42efeebcecf8190c8080a7d1f9fcd16d657b61e36db3de2a609d1";
+    image = "docker.io/andrewzah/club-nixseoul:latest@sha256:4d33ba90ef795a0a3f10ec64a61805393377a1de488a55e81aed895f834d0cfd";
     labels = {
       "traefik.enable" = "true";
       "traefik.http.routers.club-nixseoul.rule" = "Host(`nixseoul.club`)";
