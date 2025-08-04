212a482bb7bb5746e504303d9622723a2a42246e
Andrew Zah <zah@andrewzah.com>
2025-08-04 20:33:07 +0900

    update monogame shaders article
    Signed-off-by: Andrew Zah <zah@andrewzah.com>

--- hosts/falcon/containers/com-andrewzah.nix
+++ hosts/falcon/containers/com-andrewzah.nix
@@ -1,7 +1,7 @@
 {...}: {
   virtualisation.oci-containers.containers.com-andrewzah = {
     autoStart = true;
-    image = "docker.io/andrewzah/com-andrewzah:latest@sha256:268b60c6ac3fda9b998006e6eee9159e6c2d45af842c2dc343ebda996204bd07";
+    image = "docker.io/andrewzah/com-andrewzah:latest@sha256:a28fb83b08d2a2569a77d7c8ab8e2937ee116d7d8abeb1da3b512e91f538c71d";
     dependsOn = ["traefik"];
     extraOptions = ["--net=external"];
     ports = ["2020"];
