4a65b75aabe997ca657ba7d6d88ac997149eb34a
Andrew Zah <zah@andrewzah.com>
2025-09-13 20:20:02 +0900

    blanka: forwardauth, use LE staging for now

--- hosts/blanka/containers/traefik-forward-auth.nix
+++ hosts/blanka/containers/traefik-forward-auth.nix
@@ -1,10 +1,10 @@
 {config, ...}: {
   sops.secrets."traefik-forward-auth/env" = {};
 
-  virtualisation.oci-containers.containers.traefik-forward-auth = {
-    autoStart = false;
-    hostname = "forwardauth";
+  virtualisation.oci-containers.containers.forwardauth = {
     image = "docker.io/mesosphere/traefik-forward-auth:v3.2.1@sha256:7b0159e59f7b4d2ffd484371fb44f0502434991e2b8070c85460b319a8bfc035";
+    hostname = "forwardauth";
+    autoStart = false;
     environmentFiles = [config.sops.secrets."traefik-forward-auth/env".path];
     ports = ["4181"];
     extraOptions = [
@@ -16,11 +16,11 @@
       "traefik.http.routers.forwardauth.entrypoints" = "websecure";
       "traefik.http.routers.forwardauth.rule" = "Path(`/_oauth`)";
       "traefik.http.routers.forwardauth.middlewares" = "traefik-forward-auth";
-      "traefik.http.middlewares.traefik-forward-auth.forwardauth.address" = "http://forwardauth:4181";
-      "traefik.http.middlewares.traefik-forward-auth.forwardauth.authResponseHeaders" = "X-Forwarded-User";
-      "traefik.http.middlewares.traefik-forward-auth.forwardauth.trustForwardHeader" = "true";
-      "traefik.http.routers.traefik-forward-auth.tls" = "true";
-      "traefik.http.routers.traefik-forward-auth.tls.certresolver" = "cloudflare";
+      "traefik.http.middlewares.forwardauth.forwardauth.address" = "http://forwardauth:4181";
+      "traefik.http.middlewares.forwardauth.forwardauth.authResponseHeaders" = "X-Forwarded-User";
+      "traefik.http.middlewares.forwardauth.forwardauth.trustForwardHeader" = "true";
+      "traefik.http.routers.forwardauth.tls" = "true";
+      "traefik.http.routers.forwardauth.tls.certresolver" = "generic";
     };
   };
 }
