9c4d29fcda8932522eb129443227636aa4a3397c
Andrew Zah <zah@andrewzah.com>
2025-09-26 11:31:02 +0900

    update

--- hosts/blanka/containers/home-gallery.nix
+++ hosts/blanka/containers/home-gallery.nix
@@ -4,7 +4,7 @@
     hostname = "home-gallery";
     image = "docker.io/xemle/home-gallery:c2b5674a@sha256:74be27e2f8e631a3f17a3a447d3c18acbaad3beee82e261e44d4c4faeb7cfd3a";
     ports = ["32655:3000" "3000"];
-    cmd = ["run server"];
+    cmd = ["run" "server"];
     volumes = [
       "/blanka/home-gallery/data/:/data/:rw"
       "/blanka/backups/iphone-backup_2025-09-26/:/data/iphone-backup_2025-09-26/:rw"
