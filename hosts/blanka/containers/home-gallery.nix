54ff58d7cb07faf045b171c82c77238db714823d
Andrew Zah <zah@andrewzah.com>
2025-09-26 11:28:41 +0900

    update

--- hosts/blanka/containers/home-gallery.nix
+++ hosts/blanka/containers/home-gallery.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   virtualisation.oci-containers.containers.home-gallery = {
     autoStart = false;
+    hostname = "home-gallery";
     image = "docker.io/xemle/home-gallery:c2b5674a@sha256:74be27e2f8e631a3f17a3a447d3c18acbaad3beee82e261e44d4c4faeb7cfd3a";
     ports = ["32655:3000" "3000"];
     cmd = ["run server"];
