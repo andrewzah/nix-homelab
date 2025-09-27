a951f4f6a751a0c0536c11ce1fe9f1a94d9e6ba3
Andrew Zah <zah@andrewzah.com>
2025-09-28 00:11:06 +0900

    blanka: fix healthchecks port

--- hosts/blanka/containers/healthchecks.nix
+++ hosts/blanka/containers/healthchecks.nix
@@ -6,7 +6,7 @@
     image = "docker.io/healthchecks/healthchecks:v3.11.2@sha256:cd8d65cabc30bccf96ed9741f6dcfe1a93521c754cbce7af155560fda3d54526";
     environment = {
     };
-    ports = ["3000"];
+    ports = ["8000"];
     dependsOn = ["traefik"];
     extraOptions = ["--net=external"];
     environmentFiles = [config.sops.secrets."healthchecks/env".path];
@@ -17,7 +17,7 @@
       "traefik.http.routers.healthchecks.entrypoints" = "websecure";
       "traefik.http.routers.healthchecks.tls.certresolver" = "generic";
       "traefik.http.routers.healthchecks.service" = "healthchecks";
-      "traefik.http.services.healthchecks.loadbalancer.server.port" = "3000";
+      "traefik.http.services.healthchecks.loadbalancer.server.port" = "8000";
     };
   };
 }
