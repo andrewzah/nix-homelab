8aaecd55718a96fa0a9f16a0c65850b89f23926e
Andrew Zah <zah@andrewzah.com>
2025-12-21 22:36:06 -0500

    lumiere: more unfree, sops

--- hosts/lumiere/default.nix
+++ hosts/lumiere/default.nix
@@ -12,6 +12,11 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  sops.defaultSopsFile = ./secrets.yaml;
+  sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  sops.age.generateKey = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
--- hosts/lumiere/unfree.nix
+++ hosts/lumiere/unfree.nix
@@ -2,5 +2,8 @@
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "nvidia"
+      "nvidia-persistenced"
+      "nvidia-settings"
+      "nvidia-x11"
     ];
 }
