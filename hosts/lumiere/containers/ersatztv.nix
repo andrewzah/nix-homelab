cf64c7d62a79c007a51ed3ffd71472e0005dff7b
Andrew Zah <zah@andrewzah.com>
2025-12-27 14:54:57 +0900

    fix typo

--- hosts/lumiere/containers/ersatztv.nix
+++ hosts/lumiere/containers/ersatztv.nix
@@ -14,7 +14,7 @@
       ETV_STREAMING_PORT = "8410";
     };
     #environmentFiles = [config.sops.secrets."ersatztv/env".path];
-    vogumes = [
+    volumes = [
       "/lumiere/media/:/mnt/lumiere/media/:ro"
       "/lumiere/data/docker/ersatztv/config/:/config/:rw"
     ];
