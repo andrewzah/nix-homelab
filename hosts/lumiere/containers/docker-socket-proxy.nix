6aded29d63c8e424450d49a518b180f4d6b59898
Andrew Zah <zah@andrewzah.com>
2025-12-28 14:43:41 +0900

    lum: set hostname,ports for docker-socket-proxy

--- hosts/lumiere/containers/docker-socket-proxy.nix
+++ hosts/lumiere/containers/docker-socket-proxy.nix
@@ -1,8 +1,9 @@
 {config, ...}: {
   virtualisation.oci-containers.containers.docker-socket-proxy = {
     autoStart = true;
+    hostname = "socket_proxy";
     image = "ghcr.io/tecnativa/docker-socket-proxy:v0.4.1@sha256:1c211b210cf155392544face6e2c2ebfe626f97f5f1e4eea94ed2ebe2be7bc55";
-    ports = ["127.0.0.1:2375:2375"];
+    ports = ["192.168.2.5:2375:2375" "2375"];
     environment = {
       CONTAINERS = "1";
       SERVICES = "1";
