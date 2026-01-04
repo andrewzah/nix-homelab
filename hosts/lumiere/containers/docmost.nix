dba984072517a3416b63d58b4797f7f3098c70e3
Andrew Zah <zah@andrewzah.com>
2026-01-04 14:58:08 +0900

    lumiere: set secrets for docmost

--- hosts/lumiere/containers/docmost.nix
+++ hosts/lumiere/containers/docmost.nix
@@ -1,9 +1,11 @@
 {config, ...}: {
+  sops.secrets."docmost/env" = {};
   virtualisation.oci-containers.containers.docmost = {
     autoStart = true;
     image = "docker.io/docmost/docmost:0.24.1@sha256:ae9a964d58fe45071b2d87afe44ec1b0fb74a6e0124e831e906fce96b0258a84";
     ports = ["3000"];
     environment.TZ = config.time.timeZone;
+    environmentFiles = [config.sops.secrets."docmost/env".path];
     volumes = ["/lumiere/data/docker/docmost/data/:/app/data/storage/:rw"];
     dependsOn = ["traefik"];
     extraOptions = ["--net=external" "--net=internal"];
