e85d0d16f90b232132e734c0a339d9cafaea18b6
Andrew Zah <zah@andrewzah.com>
2026-01-04 13:55:32 +0900

    blanka: vaultwarden

--- hosts/blanka/containers/vaultwarden.nix
+++ hosts/blanka/containers/vaultwarden.nix
@@ -3,18 +3,17 @@
 
   virtualisation.oci-containers.containers.vaultwarden = {
     autoStart = true;
-    image = "docker.io/vaultwarden/server:1.34.3@sha256:84fd8a47f58d79a1ad824c27be0a9492750c0fa5216b35c749863093bfa3c3d7";
-    ports = ["4080:80" "3012:3012"];
-    environment = {
-      DOMAIN = "http://blanka:4080";
-      WEBSOCKET_ENABLED = "true";
-      SIGNUPS_ALLOWED = "true";
-    };
+    image = "docker.io/vaultwarden/server:1.35.1@sha256:1d43c6754a030861f960fd4dab47e1b33fc19f05bd5f8f597ab7236465a6f14b";
+    ports = [
+      "80"
+      "3012:3012"
+    ];
     environmentFiles = [config.sops.secrets."vaultwarden/env".path];
     volumes = ["/blanka/vaultwarden/:/data/:rw"];
     dependsOn = ["postgres"];
     extraOptions = [
       "--net=internal"
+      "--net=external"
 
       "--health-cmd"
       "curl -f http://localhost:80"
@@ -23,5 +22,20 @@
       "--health-retries=5"
       "--health-start-period=5s"
     ];
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.vaultwarden-ui.rule" = "Host(`bit.abare.party`)";
+      "traefik.http.routers.vaultwarden-ui.entrypoints" = "websecure";
+      "traefik.http.routers.vaultwarden-ui.tls.certresolver" = "porkbun";
+      "traefik.http.routers.vaultwarden-ui.service" = "vaultwarden-ui";
+      "traefik.http.services.vaultwarden-ui.loadbalancer.server.port" = "80";
+
+      # websockets
+      "traefik.http.routers.vaultwarden-wss.rule" = "Host(`bit.abare.party`) && Path(`/notifications/hub`)";
+      "traefik.http.routers.vaultwarden-wss.entrypoints" = "websecure";
+      "traefik.http.routers.vaultwarden-wss.tls.certresolver" = "porkbun";
+      "traefik.http.routers.vaultwarden-wss.service" = "vaultwarden-wss";
+      "traefik.http.services.vaultwarden-wss.loadbalancer.server.port" = "3012";
+    };
   };
 }
