d6deb82f83b5e18137fa361dbf128b5a07510c94
Andrew Zah <zah@andrewzah.com>
2026-01-08 16:47:13 +0900

    blanka: update static-sites

--- hosts/blanka/containers/static-sites.nix
+++ hosts/blanka/containers/static-sites.nix
@@ -1,4 +1,23 @@
 {...}: {
+  virtualisation.oci-containers.containers.com-andrewzah = {
+    autoStart = true;
+    image = "docker.io/andrewzah/com-andrewzah:2026-01-08@sha256:6352ab5520254b647b64a2b2b4ded5332c170331e24cf1c05836fd62a047ea20";
+    dependsOn = ["traefik"];
+    extraOptions = ["--net=external"];
+    ports = ["2020"];
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.com-andrewzah.rule" = "Host(`andrewzah.com`)";
+      "traefik.http.routers.com-andrewzah.entrypoints" = "websecure";
+      "traefik.http.routers.com-andrewzah.tls.certresolver" = "cloudflare";
+
+      ## anubis -- disable for now; see containers/anubis.nix
+      #"traefik.http.routers.com-andrewzah.service" = "com-andrewzah";
+      #"traefik.http.services.com-andrewzah.loadbalancer.server.port" = "2020";
+      #"traefik.http.routers.com-andrewzah.middlewares" = "anubis@docker";
+    };
+  };
+
   virtualisation.oci-containers.containers.org-scfgc = {
     autoStart = true;
     image = "docker.io/andrewzah/org-scfgc:latest@sha256:a36ddaadd8181d70df608f2676efe835026d7c01ac3dad073ed50d4e1977ebe9";
@@ -13,27 +32,8 @@
     ports = ["2020"];
   };
 
-  virtualisation.oci-containers.containers.com-andrewzah = {
-    autoStart = true;
-    image = "docker.io/andrewzah/com-andrewzah:2026-01-08@sha256:6352ab5520254b647b64a2b2b4ded5332c170331e24cf1c05836fd62a047ea20";
-    dependsOn = ["traefik"];
-    extraOptions = ["--net=external"];
-    ports = ["2020"];
-    labels = {
-      "traefik.enable" = "true";
-      "traefik.http.routers.com-andrewzah.rule" = "Host(`andrewzah.com`)";
-      "traefik.http.routers.com-andrewzah.entrypoints" = "websecure";
-      "traefik.http.routers.com-andrewzah.tls.certresolver" = "cloudflare";
-      # "traefik.http.routers.com-andrewzah.service" = "com-andrewzah";
-      # "traefik.http.services.com-andrewzah.loadbalancer.server.port" = "2020";
-
-      #  anubis -- disable for now; see containers/anubis.nix
-      # "traefik.http.routers.com-andrewzah.middlewares" = "anubis@docker";
-    };
-  };
-
   virtualisation.oci-containers.containers.club-nixseoul = {
-    autoStart = true;
+    autoStart = false;
     image = "docker.io/andrewzah/club-nixseoul:latest@sha256:4d33ba90ef795a0a3f10ec64a61805393377a1de488a55e81aed895f834d0cfd";
     labels = {
       "traefik.enable" = "true";
