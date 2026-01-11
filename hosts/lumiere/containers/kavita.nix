d030e4e915944386350c93b4a99957e41a7e76b9
Benjamin Zah <iam@benzah.com>
2026-01-10 21:39:22 -0500

    Nix Fix

--- hosts/lumiere/containers/kavita.nix
+++ hosts/lumiere/containers/kavita.nix
@@ -1,16 +1,13 @@
 {config, ...}: {
-	#sops.secrets."kavita/env" = {};
-
 	virtualisation.oci-containers.containers.kavita = {
 		autoStart = true;
 		image = "docker.io/jvmilazz0/kavita:0.8.8@ssha256:22c42f3cc83fb98b98a6d6336200b615faf2cfd2db22dab363136744efda1bb0";
 		ports = [
-			"5000:5000";
+			"5000:5000"
 		];
 		environment = {
 			TZ=config.time.timeZone;
 		};
-		#environmentFiles = [config.sops.secrets."kavita/env".path];
 		volumes = [
 			"/lumiere/data/docker/kavita/config/:/kavita/config/:rw"
 			"/lumiere/media/manga/:/manga/:ro"
