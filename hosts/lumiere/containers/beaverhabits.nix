bb3c575bf5b9ed3ef3aa9ba7a1599698d841a720
Benjamin Zah <iam@benzah.com>
2026-01-19 12:24:34 -0500

    Fix sql

--- hosts/lumiere/containers/beaverhabits.nix
+++ hosts/lumiere/containers/beaverhabits.nix
@@ -3,7 +3,7 @@
 
 	virtualisation.oci-containers.containers.beaverhabits = {
 		autoStart = true;
-    user = "1000:1000";
+    #user = "1000:1000";
 		image = "docker.io/daya0576/beaverhabits:sha-73095b0@sha256:2562eafaa4ef955a3c7a8b071a3da44168191dddf3232959d39303b5e519cd37";
 		ports = [
 			"8026:8080"
