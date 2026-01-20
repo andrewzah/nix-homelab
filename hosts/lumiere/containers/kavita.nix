05f08a39fc9051214ea8cd67210122bae509e6ed
Andrew Zah <zah@andrewzah.com>
2026-01-20 16:58:30 +0900

    lumiere: kavita 0.8.9, add manga-bw mount

--- hosts/lumiere/containers/kavita.nix
+++ hosts/lumiere/containers/kavita.nix
@@ -1,27 +1,20 @@
 {config, ...}: {
-	virtualisation.oci-containers.containers.kavita = {
-		autoStart = true;
-		image = "docker.io/jvmilazz0/kavita:0.8.8@sha256:22c42f3cc83fb98b98a6d6336200b615faf2cfd2db22dab363136744efda1bb0";
-		ports = [
-			"5000:5000"
-		];
-		environment = {
-			TZ=config.time.timeZone;
-		};
-		volumes = [
-			"/lumiere/data/docker/kavita/config/:/kavita/config/:rw"
-			"/lumiere/media/manga/:/manga/:ro"
-		];
-		extraOptions = [
-			"--net=external"
-		];
-		labels = {
-			"traefik.enable" = "true";
-			"traefik.http.routers.kavita.rule" = "Host(`kavita.lumiere.wtf`)";
-			"traefik.http.routers.kavita.entrypoints" = "websecure";
-			"traefik.http.routers.kavita.tls.certresolver" = "porkbun";
-			"traefik.http.routers.kavita.service" = "kavita";
-			"traefik.http.services.kavita.loadbalancer.server.port" = "5000";
-		};
-	};
+  virtualisation.oci-containers.containers.kavita = {
+    autoStart = true;
+    image = "docker.io/jvmilazz0/kavita:nightly-0.8.9@sha256:cb6055ce997ac6f43e0b4badd11772dfd73716b3dceaba75448fcc486e288840";
+    ports = ["5000:5000"];
+    environment.TZ = config.time.timeZone;
+    volumes = [
+      "/lumiere/data/docker/kavita/config/:/kavita/config/:rw"
+      "/lumiere/media/manga/:/manga/:ro"
+      "/lumiere/media/manga-bw/:/manga-bw/:ro"
+    ];
+    extraOptions = ["--net=external"];
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.kavita.rule" = "Host(`kavita.lumiere.wtf`)";
+      "traefik.http.routers.kavita.entrypoints" = "websecure";
+      "traefik.http.routers.kavita.tls.certresolver" = "porkbun";
+    };
+  };
 }
