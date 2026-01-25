03f63b1bbf8666b3101186fa2dadd28d1f07f335
Andrew Zah <zah@andrewzah.com>
2026-01-25 18:04:43 +0900

    blanka: knot service s/22/5555 default

--- hosts/blanka/containers/knot.nix
+++ hosts/blanka/containers/knot.nix
@@ -4,7 +4,7 @@
   virtualisation.oci-containers.containers.maedub = {
     autoStart = false;
     image = "docker.io/andrewzah/knot:0.11.0-alpha@sha256:94267add7ca92bef460c9f8f507a797c12c020c97a9c1ed3f16a09077379cb76";
-    ports = ["22" "5555"];
+    ports = ["5555" "22"];
     environment = {
       RUST_LOG = "INFO";
     };
