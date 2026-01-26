806977e5e098ce23537ee8e1b6ec02fd40693f36
Andrew Zah <zah@andrewzah.com>
2026-01-27 03:03:06 +0900

    lumiere: fix ytdl-sub paths

--- hosts/lumiere/containers/ytdl-sub.nix
+++ hosts/lumiere/containers/ytdl-sub.nix
@@ -1,6 +1,4 @@
 {config, ...}: {
-  #sops.secrets."ytdl-sub/env" = {};
-
   virtualisation.oci-containers.containers.ytdl-sub = {
     autoStart = true;
     image = "ghcr.io/jmbannon/ytdl-sub:ubuntu-2025.12.30@sha256:810fe92f46a99deda8e6b1e6cda669d2c502026ec9ce33c751a0b127f7e4459c";
@@ -13,11 +11,9 @@
       NVIDIA_DRIVER_CAPABILITIES = "all";
       NVIDIA_VISIBLE_DEVICES = "all";
     };
-    #environmentFiles = [config.sops.secrets."ytdl-sub/env".path];
     volumes = [
       "/lumiere/data/docker/ytdl-sub/config/:/config/:rw"
-      "/lumiere/media/youtube/music/:/mnt/lumiere/media/youtube/music/:rw"
-      "/lumiere/media/youtube/music_videos/:/mnt/lumiere/media/youtube/music_videos/:rw"
+      "/lumiere/media/youtube/:/mnt/lumiere/media/youtube/:rw"
     ];
     devices = ["nvidia.com/gpu=all"];
     extraOptions = ["--net=media"];
