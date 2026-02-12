a8e9e94440fa179b892385c85e0b1a7ccdc7dd41
Andrew Zah <zah@andrewzah.com>
2026-02-12 16:49:52 +0900

    lumiere: oh my god

--- hosts/lumiere/containers/zulip.nix
+++ hosts/lumiere/containers/zulip.nix
@@ -4,7 +4,10 @@
   virtualisation.oci-containers.containers.zulip = {
     autoStart = true;
     image = "ghcr.io/zulip/zulip-server:11.5-2@sha256:699259272824daf404d302ae2412b127771ff3bdc96f6406635942894994833e";
-    ports = ["80" "25"];
+    ports = [
+      "25"
+      "80"
+    ];
     environment.TZ = config.time.timeZone;
     environmentFiles = [config.sops.secrets."zulip/env".path];
     volumes = ["/lumiere/data/docker/zulip/data/:/data/:rw"];
@@ -15,7 +18,8 @@
       "traefik.http.routers.zulip.rule" = "Host(`zulip.andrewzah.com`)";
       "traefik.http.routers.zulip.entrypoints" = "websecure";
       "traefik.http.routers.zulip.tls.certresolver" = "cloudflare";
-
+      "traefik.http.routers.zulip.service" = "zulip";
+      "traefik.http.services.zulip.loadbalancer.server.port" = "80";
       #"traefik.http.routers.zulip.middlewares" = "zulip-headers";
       #"traefik.http.middlewares.zulip-headers.headers.customrequestheaders.X-Forwarded-Proto" = "https";
     };
