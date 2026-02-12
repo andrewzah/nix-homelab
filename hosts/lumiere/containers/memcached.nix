8868813a00341804d3848761333ed8b9f35d2dfb
Andrew Zah <zah@andrewzah.com>
2026-02-12 14:35:25 +0900

    lumiere: update zulip secrets for postgres

--- hosts/lumiere/containers/memcached.nix
+++ hosts/lumiere/containers/memcached.nix
@@ -3,6 +3,7 @@
 
   virtualisation.oci-containers.containers.memcached = {
     autoStart = true;
+    user = "1000:1000";
     hostname = "memcached";
     image = "docker.io/library/memcached:1.6-alpine3.23@sha256:8a82a3927694e42bc52679dc81532244de51e5faed5f9541a1283e3ad7271db1";
     ports = ["11211"];
