a7fd8ed4f8689e0e4c10a63ff466bb164a8c080a
Andrew Zah <zah@andrewzah.com>
2026-02-15 13:43:32 +0900

    blanka: matrix-conduit -> continuwuity

---
+++ hosts/blanka/containers/continuwuity.nix
@@ -0,0 +1,18 @@
+{config, ...}: {
+  sops.secrets."continuwuity/env" = {};
+
+  virtualisation.oci-containers.containers.continuwuity = {
+    autoStart = true;
+    image = "forgejo.ellis.link/continuwuation/continuwuity:latest@sha256:sha256:1f31ad44e692c9ab0a4f39a7111e8ad9dfef0f251f0a864fe05667c152f1f5c1";
+    ports = ["6167"];
+    environmentFiles = [config.sops.secrets."continuwuity/env".path];
+    volumes = ["/blanka/continuwuity/data/:/var/lib/continuwuity/:rw"];
+    extraOptions = ["--net=external"];
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.continuwuity.rule" = "Host(`matrix.andrewzah.com`)";
+      "traefik.http.routers.continuwuity.entrypoints" = "websecure";
+      "traefik.http.routers.continuwuity.tls.certresolver" = "porkbun";
+    };
+  };
+}
