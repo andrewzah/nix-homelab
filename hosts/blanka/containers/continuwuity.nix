ca0292a2047e5395b89355e93ab38d742f155b5c
Andrew Zah <zah@andrewzah.com>
2026-02-15 14:04:13 +0900

    blanka/continuwuity: use system resolver

--- hosts/blanka/containers/continuwuity.nix
+++ hosts/blanka/containers/continuwuity.nix
@@ -4,9 +4,12 @@
   virtualisation.oci-containers.containers.continuwuity = {
     autoStart = true;
     image = "forgejo.ellis.link/continuwuation/continuwuity:latest@sha256:1f31ad44e692c9ab0a4f39a7111e8ad9dfef0f251f0a864fe05667c152f1f5c1";
-    ports = ["6167"];
+    ports = ["6167" "8448"];
     environmentFiles = [config.sops.secrets."continuwuity/env".path];
-    volumes = ["/blanka/continuwuity/data/:/var/lib/continuwuity/:rw"];
+    volumes = [
+      "/blanka/continuwuity/data/:/var/lib/continuwuity/:rw"
+      "/etc/resolv.conf:/etc/resolv.conf:ro"
+    ];
     extraOptions = ["--net=external"];
     labels = {
       "traefik.enable" = "true";
